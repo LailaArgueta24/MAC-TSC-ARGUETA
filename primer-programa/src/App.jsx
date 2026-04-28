aa34fdb769bd68c28c0d5f53f21b99dc07a22365
LailaArgueta24 <319128411@pcpuma.acatlan.unam.mx>
2026-04-27 19:44:07 -0600

    primer programa en react con vite

---
+++ primer-programa/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo en React</h1>
+      <p>Prueba 1 akari</p>
+    </div>
+  );
+}
+
+export default App;
